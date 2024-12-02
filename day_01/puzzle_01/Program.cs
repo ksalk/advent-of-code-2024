b04a015f73b82af18f51711a740d67d3a36b08dc
Konrad Sałkowski <konrad.salkowski@gmail.com>
2024-12-02 12:26:36 +0100

    Day 01 - Puzzle 02 - done

--- day_01/puzzle_01/Program.cs
+++ day_01/puzzle_01/Program.cs
@@ -1,20 +1,52 @@
-﻿var inputFilePath = "input.txt";
+﻿Console.WriteLine("Puzzle 01 result:");
+await Puzzle01();
 
-var list1 = new List<int>();
-var list2 = new List<int>();
+Console.WriteLine("Puzzle 02 result:");
+await Puzzle02();
 
-await foreach (var line in File.ReadLinesAsync(inputFilePath))
+async Task<int> Puzzle01()
 {
-    var pair = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-    list1.Add(int.Parse(pair[0]));
-    list2.Add(int.Parse(pair[1]));
+    var inputFilePath = "input.txt";
+
+    var list1 = new List<int>();
+    var list2 = new List<int>();
+
+    await foreach (var line in File.ReadLinesAsync(inputFilePath))
+    {
+        var pair = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        list1.Add(int.Parse(pair[0]));
+        list2.Add(int.Parse(pair[1]));
+    }
+
+    list1 = list1.Order().ToList();
+    list2 = list2.Order().ToList();
+
+    var result = list1
+        .Zip(list2, (x, y) => Math.Abs(x - y))
+        .Sum();
+
+    Console.WriteLine(result);
+    return result;
 }
 
-list1 = list1.Order().ToList();
-list2 = list2.Order().ToList();
+async Task<int> Puzzle02()
+{
+    var inputFilePath = "input.txt";
 
-var result = list1
-    .Zip(list2, (x, y) => Math.Abs(x - y))
-    .Sum();
+    var hashset = new HashSet<int>();
+    var list2 = new List<int>();
 
-Console.WriteLine(result);
+    await foreach (var line in File.ReadLinesAsync(inputFilePath))
+    {
+        var pair = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        hashset.Add(int.Parse(pair[0]));
+        list2.Add(int.Parse(pair[1]));
+    }
+
+    var result = list2
+        .Where(x => hashset.Contains(x))
+        .Sum();
+
+    Console.WriteLine(result);
+    return result;
+}
