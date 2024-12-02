872508cd62d368ab87f91338458ba28d4387ddbb
Konrad Sałkowski <konrad.salkowski@gmail.com>
2024-12-02 12:18:48 +0100

    Day 01 - Puzzle 01 - done

---
+++ day_01/puzzle_01/Program.cs
@@ -0,0 +1,20 @@
+﻿var inputFilePath = "input.txt";
+
+var list1 = new List<int>();
+var list2 = new List<int>();
+
+await foreach (var line in File.ReadLinesAsync(inputFilePath))
+{
+    var pair = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    list1.Add(int.Parse(pair[0]));
+    list2.Add(int.Parse(pair[1]));
+}
+
+list1 = list1.Order().ToList();
+list2 = list2.Order().ToList();
+
+var result = list1
+    .Zip(list2, (x, y) => Math.Abs(x - y))
+    .Sum();
+
+Console.WriteLine(result);
