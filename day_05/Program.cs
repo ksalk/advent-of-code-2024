f9720a53ad05d74c6bce6fca97729078054f5d06
Konrad Sałkowski <konrad.salkowski@gmail.com>
2024-12-12 19:53:48 +0100

    Day 05 - Puzzle 01 - done

---
+++ day_05/Program.cs
@@ -0,0 +1,108 @@
+﻿await Puzzle01();
+
+async Task Puzzle01()
+{
+    var input = (await File.ReadAllTextAsync("input.txt")).Split("\n\n", StringSplitOptions.RemoveEmptyEntries);
+    var rules = input[0].Split("\n", StringSplitOptions.RemoveEmptyEntries);
+    var pages = input[1].Split("\n", StringSplitOptions.RemoveEmptyEntries);
+    
+    var bannedNumbers = new Dictionary<int, List<int>>();
+    foreach (var rule in rules)
+    {
+        var parts = rule.Split("|", StringSplitOptions.RemoveEmptyEntries);
+        if(bannedNumbers.ContainsKey(int.Parse(parts[1])))
+            bannedNumbers[int.Parse(parts[1])].Add(int.Parse(parts[0]));
+        else
+            bannedNumbers.Add(int.Parse(parts[1]), new List<int> { int.Parse(parts[0]) });
+    }
+
+    var middlesSum = 0;
+    foreach (var page in pages)
+    {
+        var isBanned = false;
+        var bannedNumbersForPage = new HashSet<int>();
+        var pageNumbers = page.Split(",", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+        foreach (var pageNumber in pageNumbers)
+        {
+            if (bannedNumbersForPage.Contains(pageNumber))
+            {
+                isBanned = true;
+                break;
+            }
+
+            foreach (var bannedNumber in bannedNumbers[pageNumber])
+            {
+                bannedNumbersForPage.Add(bannedNumber);
+            }
+        }
+
+        if(!isBanned)
+            middlesSum += pageNumbers[(pageNumbers.Count - 1) / 2];
+    }
+
+    Console.WriteLine(middlesSum);
+}
+
+async Task Puzzle01()
+{
+    var input = (await File.ReadAllTextAsync("input.txt")).Split("\n\n", StringSplitOptions.RemoveEmptyEntries);
+    var rules = input[0].Split("\n", StringSplitOptions.RemoveEmptyEntries);
+    var pages = input[1].Split("\n", StringSplitOptions.RemoveEmptyEntries);
+    
+    var bannedNumbers = new Dictionary<int, List<int>>();
+    foreach (var rule in rules)
+    {
+        var parts = rule.Split("|", StringSplitOptions.RemoveEmptyEntries);
+        if(bannedNumbers.ContainsKey(int.Parse(parts[1])))
+            bannedNumbers[int.Parse(parts[1])].Add(int.Parse(parts[0]));
+        else
+            bannedNumbers.Add(int.Parse(parts[1]), new List<int> { int.Parse(parts[0]) });
+    }
+
+    var middlesSum = 0;
+    foreach (var page in pages)
+    {
+        var isBanned = false;
+        var bannedNumbersForPage = new HashSet<int>();
+        var pageNumbers = page.Split(",", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+        foreach (var pageNumber in pageNumbers)
+        {
+            if (bannedNumbersForPage.Contains(pageNumber))
+            {
+                isBanned = true;
+                break;
+            }
+
+            foreach (var bannedNumber in bannedNumbers[pageNumber])
+            {
+                bannedNumbersForPage.Add(bannedNumber);
+            }
+        }
+
+        if(isBanned)
+        {
+            // attempt to fix
+            bool isFixed;
+            do
+            {
+                isFixed = true;
+                bannedNumbersForPage = new HashSet<int>();
+                foreach (var pageNumber in pageNumbers)
+                {
+                    if (bannedNumbersForPage.Contains(pageNumber))
+                    {
+                        isFixed = false;
+                        break;
+                    }
+
+                    foreach (var bannedNumber in bannedNumbers[pageNumber])
+                    {
+                        bannedNumbersForPage.Add(bannedNumber);
+                    }
+                }
+            }while(!isFixed);
+        }
+    }
+
+    Console.WriteLine(middlesSum);
+}
